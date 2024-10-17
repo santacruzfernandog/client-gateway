2e2ae41fde9262f058d28cd888cb461fe8b4a5b6
Fernando Santacruz <fsantacruz@seidoranalytics.com>
2024-10-17 09:08:48 -0300

    feat(gateway): add console log for gateway port

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
   app.useGlobalFilters(new RpcCustomExceptionFilter());
 
   await app.listen(envs.port);
-
+  console.log(`Gateway running on port: ${envs.port}`);
   logger.log(`Gateway running on port: ${envs.port}`);
 }
 bootstrap();
